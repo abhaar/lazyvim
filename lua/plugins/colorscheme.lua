1c611935caa5677026f3fdd671bc2231414674ba
abhaar <abhaar@gmail.com>
2025-05-18 19:42:42 -0400

    Add go parser to treesitter

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "navarasu/onedark.nvim",
+    opts = {
+      style = "deep",
+    },
+  },
+  -- Configure lazyvim to load the theme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+    },
+  },
+}
