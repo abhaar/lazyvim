54e811e14b15d7e44e20aac5506180bd410d0c36
abhaar <abhaar@gmail.com>
2025-05-31 11:14:13 -0400

    update default theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,16 +1,14 @@
 return {
   { "navarasu/onedark.nvim", opts = { style = "deep" } },
-  { "sainnhe/everforest" },
   { "sainnhe/gruvbox-material" },
   { "sainnhe/sonokai" },
   { "mofiqul/dracula.nvim" },
-  { "shaunsingh/nord.nvim" },
 
   -- Configure lazyvim to load the theme
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight",
+      colorscheme = "sonokai",
     },
   },
 }
