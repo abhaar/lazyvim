d2b74994d02554e73b1e98f74b9a12e6d44ce4cf
abhaar <abhaar@gmail.com>
2026-03-16 15:37:15 -0400

    update

--- lua/plugins/dap-ui.lua
+++ lua/plugins/dap-ui.lua
@@ -1,6 +1,26 @@
 return {
   "rcarriga/nvim-dap-ui",
-  opts = {},
+  opts = {
+    layouts = {
+      {
+        elements = {
+          { id = "scopes", size = 0.25 },
+          { id = "breakpoints", size = 0.25 },
+          { id = "stacks", size = 0.25 },
+          { id = "watches", size = 0.25 },
+        },
+        size = 40,
+        position = "left",
+      },
+      {
+        elements = {
+          { id = "repl", size = 1.0 },
+        },
+        size = 10,
+        position = "bottom",
+      },
+    },
+  },
   config = function(_, opts)
     local dap = require("dap")
     local dapui = require("dapui")
