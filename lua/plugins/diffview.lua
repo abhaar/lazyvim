c4b3a620769e71911b3b177e5ca156fc5e286dc0
Abhaar Gupta <abhaar.gupta@anchorlabs.com>
2026-03-16 17:56:37 +0000

    configure diffview

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -1,3 +1,27 @@
 return {
   "sindrets/diffview.nvim",
+  event = "VeryLazy",
+  keys = {
+    { "<leader>gv", "", desc = "+diffview" },
+    { "<leader>gvo", "<Cmd>DiffviewOpen<CR>", desc = "Diffview Open" },
+    { "<leader>gvc", "<Cmd>DiffviewClose<CR>", desc = "Diffview Close" },
+    { "<leader>gvt", "<Cmd>DiffviewToggleFiles<CR>", desc = "Diffview Toggle Files" },
+    { "<leader>gvr", "<Cmd>DiffviewRefresh<CR>", desc = "Diffview Refresh" },
+    { "<leader>gvf", "<Cmd>DiffviewFocusFiles<CR>", desc = "Diffview Focus Files" },
+    { "<leader>gvl", "<Cmd>DiffviewLog<CR>", desc = "Diffview Log" },
+    {
+      "<leader>gvh",
+      function()
+        Snacks.picker.files({
+          confirm = function(picker, item)
+            picker:close()
+            if item then
+              vim.cmd("DiffviewFileHistory " .. vim.fn.fnameescape(item.file))
+            end
+          end,
+        })
+      end,
+      desc = "Diffview File History",
+    },
+  },
 }
