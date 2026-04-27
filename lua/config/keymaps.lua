58742fb8b4bb91a4aac259929f7c9606b9930dff
Abhaar Gupta <abhaar.gupta@anchorlabs.com>
2026-04-27 17:06:37 +0000

    remap <leader>/ to grep cwd

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+-- Map <leader>/ to grep in the current working directory (cwd)
+vim.keymap.set("n", "<leader>/", function()
+  Snacks.picker.grep({ cwd = vim.uv.cwd() })
+end, { desc = "Grep (cwd) " })
